bbdcb98ecb7dddfd1130ca1f7eb5181b0bc81e79
sirothiyapradhumn <sirothiyapradhumn@gmail.com>
2024-10-15 16:18:39 +0530

    Added Drawer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import LeftNav from './LeftNav'
 import { CanvasContextProvider } from './context/CanvasContext'
 import { ReactFlowProvider } from '@xyflow/react';
 import styles from './index.module.css';
+import MiniDrawer from './MiniDrawer';
 
 const App = () => {
   return (
     <div className={styles.canvasModule}>
+      <MiniDrawer/>
       <LeftNav />
       <Canvas />
     </div>
